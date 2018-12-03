8c96d8e03748df6793730daf71ef0276e8eda533
brunoalvesdasilvabr <brunoalvesdasilvabr@gmail.com>
2018-12-03 10:28:05 -0200

    cart app

---
+++ src/Components/Cart/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import CartHeader from '../CartHeader';
+const Cart = ()=>(
+    <div>
+        <p>Carrinho</p>
+        <CartHeader></CartHeader>
+    </div>
+)
+
+export default Cart;
